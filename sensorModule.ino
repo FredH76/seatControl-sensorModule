daee589be1d6d73fe0d89d46ddeeb3f478a23882
fredH76 <frederichuguenin76@gmail.com>
2020-06-25 11:45:06 +0200

    SERIAL + EEPROM okay

--- sensorModule.ino
+++ sensorModule.ino
@@ -1,19 +1,25 @@
-#include <EEPROM.h>
-#include <SoftwareSerial.h>
+////////////////////////////////////////////////////////////////////////////////////////////////
+//    This code is for ESP 32 card ( <=> #if defined(ESP_H))
+//    and make use of BluetoothSerial.h
+//    otherwise you shoudh use SoftwareSerial.h + update begin() method accrodingly
+////////////////////////////////////////////////////////////////////////////////////////////////
 
-const int SR04T_DISCONNECTED = -1;
-enum SIDE {
+#include <EEPROM.h>
+#include "BluetoothSerial.h"
+
+// define the number of bytes to use
+#define EEPROM_SIZE 32
+
+const int SR04T_DISCONNECTED = -1; // UltraSonar Sensor disconnected
+enum SIDE
+{ // use bit identification
   LEFT = 1,
-  FRONT= 2,
-  RIGHT= 4
+  FRONT = 2,
+  RIGHT = 4
 };
 
 ////////////////////////////////////      PIN CONFIGURATION    /////////////////////////////////
-// BLE HC-05 pin
-const int txBT = 14; // TX3
-const int rxBT = 15; // RX3
-
-// DIGITAL 
+// DIGITAL
 const int trigRIGHT = 2;
 const int echoRIGHT = 3;
 const int trigFRONT = 4;
@@ -21,91 +27,98 @@ const int echoFRONT = 5;
 const int trigLEFT = 6;
 const int echoLEFT = 7;
 
-const int buzzerPlus = 10;
-const int buzzerGround = 9;
-const int led = 13;
+const int buzzerPlus = 18;
+const int buzzerGround = 19;
+const int led = 16;
 
 // ANALOG
+// none for now ...
 
 ////////////////////////////////////      COMMAND HEX CODE      /////////////////////////////////
 const int CONFIG_STOP_DURATION = 0xC1;
 const int EMERGENCY_STOP = 0xE0;
 
 ////////////////////////////////////      VAR DECLARATION      /////////////////////////////////
-
-//   EEPROM adress       
+//  EEPROM adress to save configuration param
 int eeAdr_stopDuration = 0;
 int eeAdr_sensorFlags = 2;
-int eeAdr_leftThreshold  = 10;
+int eeAdr_leftThreshold = 10;
 int eeAdr_frontThreshold = 11;
 int eeAdr_rightThreshold = 12;
 
-SoftwareSerial btSerial = SoftwareSerial(rxBT, txBT);
-int eeVal;        // value read in EEPROM
-int stopDuration; // time before releasing control to joystick (in ms)
+//  BLUETOOTH SERIAL
+BluetoothSerial btSerial;
+
+//  GENERAL
+int eeVal;           // value read in EEPROM
+int stopDuration;    // time before releasing control to joystick (in ms)
 int sensorFlags = 0; // LEFT < FRONT < RIGHT < ...
 int leftThreshold;
 int frontThreshold;
 int rightThreshold;
-int buzzFreq;     // frequency (in Hz)
-int btByte;       // store byte to be read from BLE serial
+int buzzFreq; // frequency (in Hz)
+int btByte;   // store byte to be read from BLE serial
 
 /***********************************************************************************************
  * setUp()
 ***********************************************************************************************/
 void setup()
 {
-  Serial.begin(9600);
 
   // configure pin INPUT/OUTPUT
-  pinMode(txBT, OUTPUT);
-  pinMode(rxBT, INPUT);
-  pinMode(trigRIGHT, OUTPUT);
+  /*pinMode(trigRIGHT, OUTPUT);
   pinMode(echoRIGHT, INPUT);
   pinMode(trigFRONT, OUTPUT);
   pinMode(echoFRONT, INPUT);
   pinMode(trigLEFT, OUTPUT);
-  pinMode(echoLEFT, INPUT);
+  pinMode(echoLEFT, INPUT);*/
   pinMode(buzzerPlus, OUTPUT);
   pinMode(buzzerGround, OUTPUT);
   pinMode(led, OUTPUT);
 
+  // LED OFF
+  digitalWrite(led, HIGH);
+
+
+  // start Serial for debbuging
+  Serial.begin(9600);
+
+  // init BLUETOOTH
+  Serial.println(F("Initializing BlueTooth ..."));
+  btSerial.begin("WheelChair Sensor");
+
+  // set BUZZER Frequence
+  buzzFreq = 6000; // frequency (in Hz)
+
+  // initialize EEPROM with predefined size
+  EEPROM.begin(EEPROM_SIZE);
+
   // LOAD TEST DATA into EEPROM //
-  EEPROM.write(eeAdr_sensorFlags,LEFT | FRONT | RIGHT );
-  EEPROM.write(eeAdr_leftThreshold,22);
-  EEPROM.write(eeAdr_frontThreshold,30);
-  EEPROM.write(eeAdr_rightThreshold,40);
+  EEPROM.write(eeAdr_sensorFlags, LEFT | FRONT | RIGHT);
+  EEPROM.write(eeAdr_leftThreshold, 22);
+  EEPROM.write(eeAdr_frontThreshold, 30);
+  EEPROM.write(eeAdr_rightThreshold, 40);
+  EEPROM.commit();
 
-
+  // load CONFIGURATION from EEPROM storage
   // load Stop duration from EEPROM (default 3sec)
   eeVal = EEPROM.read(eeAdr_stopDuration);
-  (eeVal<0 || eeVal > 10)  ? stopDuration = 3000 : stopDuration = eeVal * 1000;
-
+  (eeVal < 0 || eeVal > 10) ? stopDuration = 3000 : stopDuration = eeVal * 1000;
   // load Sensor Flag Configuration (default 8 sensors activated <=> 256)
   eeVal = EEPROM.read(eeAdr_sensorFlags);
-  (eeVal<0 || eeVal > 256) ? sensorFlags = 256 :  sensorFlags = eeVal;
-
-  // load LEFT threshold from EEPROM (default 2m)
+  (eeVal < 0 || eeVal > 256) ? sensorFlags = 256 : sensorFlags = eeVal;
+  // load LEFT threshold from EEPROM (default 1m)
   eeVal = EEPROM.read(eeAdr_leftThreshold);
-  (eeVal<15 || eeVal > 200) ? leftThreshold = 2000 :  leftThreshold = eeVal*10;
-
-  // load FRONT threshold from EEPROM (default 2m)
+  (eeVal < 15 || eeVal > 200) ? leftThreshold = 1000 : leftThreshold = eeVal * 10;
+  // load FRONT threshold from EEPROM (default 1m)
   eeVal = EEPROM.read(eeAdr_frontThreshold);
-  (eeVal<15 || eeVal > 200) ? frontThreshold = 2000 :  frontThreshold = eeVal*10;
-
-  // load RIGHT threshold from EEPROM (default 2m)
+  (eeVal < 15 || eeVal > 200) ? frontThreshold = 1000 : frontThreshold = eeVal * 10;
+  // load RIGHT threshold from EEPROM (default 1m)
   eeVal = EEPROM.read(eeAdr_rightThreshold);
-  (eeVal<15 || eeVal > 200) ? rightThreshold = 2000 :  rightThreshold = eeVal*10;
-
-  // init BLE
-  Serial.println(F("Initializing BLE ..."));
-  btSerial.begin(9600);  
-  
-  // set Buzzer Frequence
-  buzzFreq = 6000;     // frequency (in Hz)
+  (eeVal < 15 || eeVal > 200) ? rightThreshold = 1000 : rightThreshold = eeVal * 10;
 
   // test buzzer and relay
-  testUSsensor();
+  //testUSsensor();
 }
 
 /***********************************************************************************************
@@ -116,12 +129,41 @@ void loop()
   int res;
 
   // read from BLE
-  while (btSerial.available()){
+  while (btSerial.available())
+  {
     btByte = btSerial.read();
     delay(3);
-    // TODO : 
+    Serial.print(F("received : Ox"));
+    Serial.println(btByte);
+
+    switch (btByte)
+    {
+    case CONFIG_STOP_DURATION:
+      // get stop duration from BLE
+      btByte = btSerial.read();
+      Serial.print(F("received : Ox"));
+      Serial.println(btByte);     //check if value is in range 1 to 10
+      if (btByte < 0 || btByte > 10)
+        btByte = 3;
+      //save new value in EEPROM
+      EEPROM.write(eeAdr_stopDuration, btByte);
+      EEPROM.commit();
+      // update local stopDuration (and convert into millis)
+      stopDuration = btByte * 1000;
+      break;
+
+    case EMERGENCY_STOP:
+      // launch emergency procedure
+      emergencyProcedure();
+      // clear input buffer
+      while (btSerial.available())
+        btSerial.read();
+      break;
+    }
   }
 
+
+/*
   // read LEFT SENSOR
   if(sensorFlags & LEFT){
     res = getDistByTrig(trigLEFT,echoLEFT);
@@ -141,64 +183,83 @@ void loop()
     res = getDistByTrig(trigRIGHT,echoRIGHT);
     if(res > 0 && res < rightThreshold)
       emergencyProcedure();
-  }
+  }*/
 }
 
 /***********************************************************************************************
  * Emergency Procedure to Stop Elec wheelchair
 ***********************************************************************************************/
-void emergencyProcedure(){
+void tone(byte pin, int freq, int duration)
+{
+  digitalWrite(led, LOW);
 
-    /* for TEST : buzz 3 times
-    for(int i = 0 ; i < 3; i++){
-        tone(buzzerPlus, buzzFreq, 100);
-        digitalWrite(led,HIGH);
-        delay(200);        
-        digitalWrite(led,LOW);
-        delay(200);        
-    }*/ 
+  ledcSetup(0, 2000, 8);  // setup beeper
+  ledcAttachPin(pin, 0);  // attach beeper
+  ledcWriteTone(0, freq); // play tone
+  delay(duration);
+  ledcWriteTone(0, 0); // stop tone
+
+  digitalWrite(led, HIGH);
+}
+
+void emergencyProcedure()
+{
+  // for TEST : buzz 3 times
+  for (int i = 0; i < 3; i++)
+  {
     tone(buzzerPlus, buzzFreq, 100);
-
-
+    digitalWrite(led, LOW);
+    delay(200);
+    digitalWrite(led, HIGH);
+    delay(200);
+  }
 };
-
 
 ///////////////////////////////////    ULTRA SONIC FUNCTIONS   /////////////////////////////////
 /***********************************************************************************************
  * TEST US SENSOR
 ***********************************************************************************************/
-int testUSsensor(){
+int testUSsensor()
+{
   int res;
 
   Serial.println("_________________ test SENSOR ____________________________");
-  
-  res= getDistByTrig(trigLEFT,echoLEFT);
-  if(res>0){
+
+  res = getDistByTrig(trigLEFT, echoLEFT);
+  if (res > 0)
+  {
     Serial.print("SUCCESS: LEFT  ultra sonar measure = ");
-    Serial.print(((float)res) /10); Serial.println(" cm");
-  } else if (res == SR04T_DISCONNECTED) 
-        Serial.println("ERROR: LEFT Ultra sonar SENSOR is not connected");
-      else
-        Serial.println("ERROR: LEFT Ultra sonar MODULE is not connected");
+    Serial.print(((float)res) / 10);
+    Serial.println(" cm");
+  }
+  else if (res == SR04T_DISCONNECTED)
+    Serial.println("ERROR: LEFT Ultra sonar SENSOR is not connected");
+  else
+    Serial.println("ERROR: LEFT Ultra sonar MODULE is not connected");
 
-  res= getDistByTrig(trigFRONT,echoFRONT);
-  if(res>0){
+  res = getDistByTrig(trigFRONT, echoFRONT);
+  if (res > 0)
+  {
     Serial.print("SUCCESS: FRONT ultra sonar measure = ");
-    Serial.print(((float)res) /10); Serial.println(" cm");
-  } else if (res == SR04T_DISCONNECTED) 
-        Serial.println("ERROR: FRONT Ultra sonar SENSOR is not connected");
-      else
-        Serial.println("ERROR: FRONT Ultra sonar MODULE is not connected");
+    Serial.print(((float)res) / 10);
+    Serial.println(" cm");
+  }
+  else if (res == SR04T_DISCONNECTED)
+    Serial.println("ERROR: FRONT Ultra sonar SENSOR is not connected");
+  else
+    Serial.println("ERROR: FRONT Ultra sonar MODULE is not connected");
 
-  res= getDistByTrig(trigRIGHT,echoRIGHT);
-  if(res>0){
+  res = getDistByTrig(trigRIGHT, echoRIGHT);
+  if (res > 0)
+  {
     Serial.print("SUCCESS: RIGHT ultra sonar measure = ");
-    Serial.print(((float)res) /10); Serial.println(" cm");
-  } else if (res == SR04T_DISCONNECTED) 
-        Serial.println("ERROR: RIGHT Ultra sonar SENSOR is not connected");
-      else
-        Serial.println("ERROR: RIGHT Ultra sonar MODULE is not connected");
-
+    Serial.print(((float)res) / 10);
+    Serial.println(" cm");
+  }
+  else if (res == SR04T_DISCONNECTED)
+    Serial.println("ERROR: RIGHT Ultra sonar SENSOR is not connected");
+  else
+    Serial.println("ERROR: RIGHT Ultra sonar MODULE is not connected");
 }
 
 /***********************************************************************************************
@@ -218,8 +279,9 @@ int getDistByTrig(int trigPin, int echoPin)
   period = pulseIn(echoPin, HIGH);
 
   // patch for SR04T
-  if (period >20000) return SR04T_DISCONNECTED;
-  
+  if (period > 20000)
+    return SR04T_DISCONNECTED;
+
   //delay(30); // wait for signal to come back
   return (period * 343 / 2000);
 }
