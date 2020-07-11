c89dda58a2b35b37c01cdfe063cc28e3639ac67c
fredH76 <frederichuguenin76@gmail.com>
2020-07-11 15:25:10 +0200

    remove useless scan function

--- sensorModule.ino
+++ sensorModule.ino
@@ -227,44 +227,6 @@ void emergencyProcedure()
   }
 };
 
-///////////////////////////////////       BLE FUNCTIONS        /////////////////////////////////
-/*class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
-{
-
-  // Called for each BLE Advertised Device found.
-  void onResult(BLEAdvertisedDevice advertisedDevice)
-  {
-    Serial.print("BLE Advertised Device found: ");
-    Serial.println(advertisedDevice.toString().c_str());
-
-    // if the device is the joystickDevice => stop scan
-    // if (advertisedDevice.getName() == "BLE Joystick")
-    if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(SERVICE_UUID))
-    {
-      BLEDevice::getScan()->stop();
-      joystickDevice = new BLEAdvertisedDevice(advertisedDevice);
-      joystickFound = true;
-    }
-  }
-};
-
-/***********************************************************************************************
- * Scan for BLE devices and select joystickDevice (if any).
-***********************************************************************************************
-void scanForJoystick()
-{
-  BLEDevice::init("");
-  // Retrieve a Scanner and set the callback we want to use to be informed when we
-  // have detected a new device.  Specify that we want active scanning and start the
-  // scan to run for 5 seconds.
-  BLEScan *pBLEScan = BLEDevice::getScan();
-  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-  //pBLEScan->setInterval(1349);
-  //pBLEScan->setWindow(449);
-  pBLEScan->setActiveScan(true);
-  pBLEScan->start(5, false);
-}*/
-
 /***********************************************************************************************
  *  BLE callbacks
 ***********************************************************************************************/
